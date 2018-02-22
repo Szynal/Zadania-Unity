9a214ce11a349410d1512f6ec847ef48e5df21c2
Paweł Szynal <pawel.szynal@email.com>
2018-02-22 20:40:35 +0100

    Zadanie 2

---
+++ Zadania - Unity Xi4G/Assets/Scripts/Classes/HyperLinks.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class HyperLinks : MonoBehaviour
+    {
+        public void OpenURL()
+        {
+            Application.OpenURL("https://github.com/Szynal/Zadania-Unity");
+        }
+    }
+}
